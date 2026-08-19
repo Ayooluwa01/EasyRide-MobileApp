ebe6974e9774785b55c73d22fc163f6a9f9ea90a
stephen <Olusegunstephen222@gmail.com>
2026-08-19 01:21:21 +0100

    bumped androif version to 35

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,12 +1,57 @@
+// plugins {
+//     id("com.android.application")
+//     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+//     id("dev.flutter.flutter-gradle-plugin")
+// }
+
+// android {
+//     namespace = "com.example.easy_ride"
+//     compileSdk = flutter.
+//     ndkVersion = flutter.ndkVersion
+
+//     compileOptions {
+//         sourceCompatibility = JavaVersion.VERSION_17
+//         targetCompatibility = JavaVersion.VERSION_17
+//     }
+
+//     defaultConfig {
+//         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+//         applicationId = "com.example.easy_ride"
+//         // You can update the following values to match your application needs.
+//         // For more information, see: https://flutter.dev/to/review-gradle-config.
+//         minSdk = flutter.minSdkVersion
+//         targetSdk = flutter.targetSdkVersion
+//         versionCode = flutter.versionCode
+//         versionName = flutter.versionName
+//     }
+
+//     buildTypes {
+//         release {
+//             // TODO: Add your own signing config for the release build.
+//             // Signing with the debug keys for now, so `flutter run --release` works.
+//             signingConfig = signingConfigs.getByName("debug")
+//         }
+//     }
+// }
+
+// kotlin {
+//     compilerOptions {
+//         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+//     }
+// }
+
+// flutter {
+//     source = "../.."
+// }
 plugins {
     id("com.android.application")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
     namespace = "com.example.easy_ride"
-    compileSdk = 37
+    
+    compileSdk = 35 
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -15,20 +60,17 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.easy_ride"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        
+        // FIXED: Hardcoded to match the compilation environment targeting level 35
+        targetSdk = 35 
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
